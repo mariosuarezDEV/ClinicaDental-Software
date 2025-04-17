749039aa4ffd5b3646b2eaea1063dac5d854bf9a
Luis Mario Suárez <luismariosuarez@lumace.cloud>
2025-04-16 18:07:04 -0600

    Agregar configuración de Docker y archivos de requisitos, incluyendo soporte para MySQL y dotenv.

--- expendientes/settings.py
+++ expendientes/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ws!6%s_2sav_yv!z9i-(%sw9lgt6=$m_)abb^v*%&s&c8dr^p%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True')
 
 ALLOWED_HOSTS = ['*']
 
@@ -100,10 +105,11 @@ WSGI_APPLICATION = 'expendientes.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.getenv("MYSQL_PUBLIC_URL"),
+        engine="django.db.backends.mysql",
+        conn_max_age=600,  # 🏆 Mantiene conexiones abiertas por 10 minutos
+    )
 }
 
 
